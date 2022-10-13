27b5f239077e51e94a2f2947a6ea1cb85407c7ac
Yan Maia <yandamasceno01@gmail.com>
2022-10-12 22:55:57 -0300

    adicao da dependencia junit

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -27,16 +27,16 @@ public class LocacaoService {
 		
 		return locacao;
 	}
-    //Criação de teste básico sem ferramenta
+    //Criacao de teste basico sem ferramenta
 	public static void main(String[] args) {
-		//cenário -> Inicializamos o que precisamos
+		//cenario -> Inicializamos o que precisamos
 		LocacaoService service = new LocacaoService(); //Instância do método que queremos testar
 		Usuario usuario = new Usuario();
 		Filme filme = new Filme("Filme 1", 2,5.0);
-		//ação
+		//acao
 	    Locacao locacao = service.alugarFilme(usuario, filme); //retorna um objeto locação
 
-		//verificação
+		//verificacao
 		System.out.println(locacao.getValor());
 		System.out.println(locacao.getDataLocacao());
 		System.out.println(locacao.getDataRetorno());
